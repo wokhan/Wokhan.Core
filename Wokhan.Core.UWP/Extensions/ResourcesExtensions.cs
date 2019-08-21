a0c5048653ebb3b38c39e571526c361ab0db3a93
Khan <wokhan@outlook.com>
2019-08-21 08:57:31 +0200

    Renamed projects and fixed dependencies

--- Wokhan.Core.UWP/Extensions/ResourcesExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Wokhan.UWP.Extensions
-{
-    public static partial class ResourcesExtensions
-    {
-        public static string TranslateProperty([CallerMemberName] string propName = null) => propName.Translate();
-    }
-}
