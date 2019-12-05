e8df733b408be2ea6ccc365f9692977df501392d
Jérôme Saliba <j.saliba@outlook.com>
2019-12-05 12:40:04 +0000

    Added GenericComparer.cs

---
+++ Wokhan.Core/Comparers/GenericComparer.cs
@@ -0,0 +1,11 @@
+ namespace Wokhan.Core.Comparers 
+ {
+    private class GenericComparer<T, TK> : IEqualityComparer<T>
+    {
+        private Func<T, TK> keyGetter;
+
+        public GenericComparer(Func<T, TK> keyGetter) => this.keyGetter = keyGetter;
+        public bool Equals(T x, T y) => keyGetter(x)?.Equals(keyGetter(y)) ?? false;
+        public int GetHashCode(T obj) => keyGetter(obj)?.GetHashCode() ?? 0;
+    }
+}
