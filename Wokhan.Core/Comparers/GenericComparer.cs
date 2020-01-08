9ff9002acccab740963047a981a447f26c93a398
Jérôme Saliba <j.saliba@outlook.com>
2020-01-08 16:39:17 +0000

    Updated GenericComparer.cs

--- Wokhan.Core/Comparers/GenericComparer.cs
+++ Wokhan.Core/Comparers/GenericComparer.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace Wokhan.Core.Comparers 
 {
-    public class GenericComparer<T, TK> : IEqualityComparer<T>
+    public class GenericComparer<T> : IEqualityComparer<T>
     {
-        private Func<T, TK> keyGetter;
+        private Func<T, object> keyGetter;
 
-        public GenericComparer(Func<T, TK> keyGetter) => this.keyGetter = keyGetter;
+        public GenericComparer(Func<T, object> keyGetter) => this.keyGetter = keyGetter;
         public bool Equals(T x, T y) => keyGetter(x)?.Equals(keyGetter(y)) ?? false;
         public int GetHashCode(T obj) => keyGetter(obj)?.GetHashCode() ?? 0;
     }
