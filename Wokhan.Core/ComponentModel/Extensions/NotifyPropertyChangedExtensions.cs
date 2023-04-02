7d25735be3f6eaec92cee98befb38e6927ed85aa
Jérôme Saliba <wokhan@outlook.com>
2023-04-02 15:32:28 +0200

    Rolling back to use reflection on GetOrSetValueAsync extension method, as playing around with Unsafe was... well... unsafe (and unstable).

--- Wokhan.Core/ComponentModel/Extensions/NotifyPropertyChangedExtensions.cs
+++ Wokhan.Core/ComponentModel/Extensions/NotifyPropertyChangedExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -52,14 +53,15 @@ public static class NotifyPropertyChangedExtensions
     /// <typeparam name="T"></typeparam>
     /// <param name="src"></param>
     /// <param name="resolve"></param>
-    /// <param name="field"></param>
+    /// <param name="targetField"></param>
     /// <param name="propertyChanged"></param>
-    /// <param name="propertyName"></param>
+    /// <param name="fieldName">For internal use (for reflection, based on the 'targetField' parameter)</param>
+    /// <param name="propertyName">For internal use (for reflection, based on the 'targetField' parameter)</param>
     /// <returns></returns>
-    public static T? GetOrSetValueAsync<T>(this INotifyPropertyChanged src, Func<T> resolve, ref T field, Action<string>? propertyChanged = null, [CallerMemberName] string? propertyName = null)
+    public static T? GetOrSetValueAsync<T>(this INotifyPropertyChanged src, Func<T> resolve, ref T targetField, Action<string>? propertyChanged = null, [CallerArgumentExpression(nameof(targetField))] string? fieldName = null, [CallerMemberName] string? propertyName = null)
     {
         // TODO: not sure about that. Wrapping the resolver in a task wrapper in a function only to have it run async looks a bit too much.
-        return GetOrSetValueAsync(src, () => Task.Run(() => resolve()), ref field, propertyChanged, propertyName);
+        return GetOrSetValueAsync(src, () => Task.Run(() => resolve()), ref targetField, propertyChanged, propertyName);
     }
 
     /// <summary>
@@ -70,32 +72,50 @@ public static class NotifyPropertyChangedExtensions
     /// public ImageSource? Image 
     /// {
     ///     get => this.GetOrSetValueAsync(() => myAsyncMethod(), ref image, OnPropertyChanged);
-    ///     set => image = value; // Optional since the value is set when first called
+    ///     set => image = value; // Optional (and usually useless since the value is set when the getter gets called for the first time)
     /// }
     /// </code>
+    /// Note: if the getter is called multiple times while the computed value is null, the async callback will be called again. You should handle this to avoid this behavior.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="src"></param>
-    /// <param name="resolveAsync"></param>
-    /// <param name="field"></param>
+    /// <param name="resolveAsync">Async factory to resolve the value</param>
+    /// <param name="targetField"></param>
     /// <param name="propertyChanged"></param>
-    /// <param name="propertyName"></param>
+    /// <param name="fieldName">For internal use (for reflection, based on the 'targetField' parameter)</param>
+    /// <param name="propertyName">For internal use (for reflection, based on the 'targetField' parameter)</param>
     /// <returns></returns>
-    public unsafe static T? GetOrSetValueAsync<T>(this INotifyPropertyChanged src, Func<Task<T>> resolveAsync, ref T field, Action<string>? propertyChanged = null, [CallerMemberName] string? propertyName = null)
+    public unsafe static T? GetOrSetValueAsync<T>(this INotifyPropertyChanged src, Func<Task<T>> resolveAsync, ref T targetField, Action<string>? propertyChanged = null, [CallerArgumentExpression(nameof(targetField))] string? fieldName = null, [CallerMemberName] string? propertyName = null)
     {
-        if (field is not null)
+        // Dangerous code as it doesn't ensure field has been pinned in memory, resulting in memory violations.
+        // Keeping it in case someone has an idea.
+        //if (field is not null)
+        //{
+        //    return field;
+        //}
+
+        //var ptr = Unsafe.AsPointer(ref field);
+        //_ = resolveAsync().ContinueWith(task =>
+        //{
+        //    Unsafe.Write(ptr, task.Result);
+        //    propertyChanged?.Invoke(propertyName);
+        //});
+
+
+        //return field;
+
+
+        fieldName ??= $"<{propertyName}>k_BackingField"; // TODO: check how this is generated. Could be broken with some .NET future implementations.
+        var fieldInfo = src.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+        if (fieldInfo is null)
         {
-            return field;
+            throw new ArgumentOutOfRangeException(fieldName);
         }
-
-        var ptr = Unsafe.AsPointer(ref field);
-        _ = resolveAsync().ContinueWith(task =>
+        if (targetField is null)
         {
-            Unsafe.Write(ptr, task.Result);
-            propertyChanged?.Invoke(propertyName);
-        });
-
-
-        return field;
+            //TODO: check why we are using TaskScheduler.Current and not Default (might cause a thread issue)
+            _ = resolveAsync().ContinueWith(task => { fieldInfo.SetValue(src, task.Result); propertyChanged?.Invoke(propertyName!); }, TaskScheduler.Current);
+        }
+        return targetField;
     }
 }
